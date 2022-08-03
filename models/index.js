f69bdfd5df8c469ae84646f1aa692592d160dbb7
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-08-03 11:21:56 -0400

    updated model index

--- models/index.js
+++ models/index.js
@@ -11,6 +11,24 @@ const ProductTag = require('./ProductTag');
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Category);
+
+Category.hasMany(Product);
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
+
 
 module.exports = {
   Product,
